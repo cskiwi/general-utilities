f3c3da464c19daf75d3b7673478baec30753b956
cskiwi <glenn.latomme@gmail.com>
2015-12-01 13:20:55 +0100

    stuff

--- app.js
+++ app.js
@@ -9,24 +9,24 @@ angular.module('myApp', [
 'ngRoute', 'ngSanitize', 'ngTouch',		//additional angular modules
 'cskiwi.general-utilities'
 ]).
-config(['$routeProvider', '$locationProvider', '$compileProvider', function($routeProvider, $locationProvider, $compileProvider) {
-	/**
+config(['$routeProvider', '$locationProvider', '$compileProvider', function ($routeProvider, $locationProvider, $compileProvider) {
+    /**
 	setup - whitelist, appPath, html5Mode
 	@toc 1.
 	*/
-	$locationProvider.html5Mode(false);		//can't use this with github pages / if don't have access to the server
-	
-	// var staticPath ='/';
-	var staticPath;
-	// staticPath ='/angular-services/general-utilities/';		//local
-	// staticPath ='/';		//nodejs (local)
-	staticPath ='/general-utilities/';		//gh-pages
-	var appPathRoute ='/';
-	var pagesPath =staticPath+'pages/';
-	
-	
-	$routeProvider.when(appPathRoute+'home', {templateUrl: pagesPath+'home/home.html'});
+    $locationProvider.html5Mode(false);		//can't use this with github pages / if don't have access to the server
 
-	$routeProvider.otherwise({redirectTo: appPathRoute+'home'});
-	
+    // var staticPath ='/';
+    var staticPath;
+    // staticPath ='/angular-services/general-utilities/';		//local
+    // staticPath ='/';		//nodejs (local)
+    staticPath = '/general-utilities/';		//gh-pages
+    var appPathRoute = '/';
+    var pagesPath = staticPath + 'pages/';
+
+
+    $routeProvider.when(appPathRoute + 'home', { templateUrl: pagesPath + 'home/home.html' });
+
+    $routeProvider.otherwise({ redirectTo: appPathRoute + 'home' });
+
 }]);
