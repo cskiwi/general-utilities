3657e8a150b8593063a058d41f63f45fb2098dc6
cskiwi <glenn.latomme@gmail.com>
2015-12-01 15:10:15 +0100

    Added live update

--- pages/home/HomeCtrl.js
+++ pages/home/HomeCtrl.js
@@ -4,7 +4,7 @@
 'use strict';
 
 angular.module('general-utilities-demo').controller('HomeCtrl', function ($scope, $rootScope, cskiwiUtilities) {
-    $scope.networkInfo = cskiwiUtilities.networkInfo();
-    $scope.deviceInfo = cskiwiUtilities.deviceInfo();
+    $scope.networkInfo = cskiwiUtilities.networkInfo;
+    $scope.deviceInfo = cskiwiUtilities.deviceInfo;
 
 });
