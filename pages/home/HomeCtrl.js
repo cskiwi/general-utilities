1942121e848544581307c51b58546186e3eb5b2c
cskiwi <glenn.latomme@gmail.com>
2015-12-01 13:04:06 +0100

    'init'

---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'cskiwiUtilities', function($scope, cskiwiUtilities) {
+	//TODO - put any directive code here
+}]);
